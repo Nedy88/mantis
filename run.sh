f8928de110e708170b60463baead1d88c8978bc2
Nedy88 <nedy88@gmail.com>
2024-12-06 16:53:19 +0200

    Debug slurm not starting.

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 bash scripts/install_pixi.sh
 source ~/.bashrc
 cd /home/nedyalko_prisadnikov/mantis
+echo "Running pixi install."
 pixi install
 pixi run torchrun --nnodes=1 --nproc-per-node=8 src/train.py "$@"
